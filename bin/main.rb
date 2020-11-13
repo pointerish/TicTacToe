57dffb7947dcbfef45911d64832a6bf908f96452
Josias Alvarado <josiasjag@gmail.com>
2020-11-13 13:45:56 -0600

    Create barebones user interaction and prompts.

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
 
-puts 'Hello, World!'
+def game_flow
+  puts 'Tic Tac Toe Game Started!'
+  9.times do |turn| # There are only 9 possible moves until draw
+    puts 'Select where you want to move: '
+    move_selection = gets.chomp
+    # All the board manipulation and checks happen here
+    puts "Your move selection was #{move_selection}."
+    puts "Showing board."
+  end
+end
+
+game_flow()
