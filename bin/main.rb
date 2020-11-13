afa71ac76a5a22de14786b199a5b50203a403047
Josias Alvarado <josiasjag@gmail.com>
2020-11-13 13:50:46 -0600

    Fix linter errors.

--- bin/main.rb
+++ bin/main.rb
@@ -3,12 +3,12 @@
 def game_flow
   puts 'Tic Tac Toe Game Started!'
   9.times do |turn| # There are only 9 possible moves until draw
-    puts 'Select where you want to move: '
+    puts "Turn #{turn}-> Select where you want to move: "
     move_selection = gets.chomp
     # All the board manipulation and checks happen here
     puts "Your move selection was #{move_selection}."
-    puts "Showing board."
+    puts 'Showing board.'
   end
 end
 
-game_flow()
+game_flow
