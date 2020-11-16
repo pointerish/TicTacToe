1d398390d0c01b9e1d4afd26c5e6f42284fc0c8f
Josias Alvarado <josiasjag@gmail.com>
2020-11-16 10:24:28 -0600

    Made improvements requested by the TSE.

--- bin/main.rb
+++ bin/main.rb
@@ -1,21 +1,22 @@
 #!/usr/bin/env ruby
 
 def game_flow
-  puts 'Tic Tac Toe Game Started!'
-  puts 'Enter name for player X: '
+  puts "Tic Tac Toe Game Started!\n\n"
+  print 'Enter name for player X: '
   player_x = gets.chomp
-  puts 'Enter name for player O: '
+  print 'Enter name for player O: '
   player_o = gets.chomp
-  puts "Welcome to TicTacToe, #{player_x} and #{player_o}!"
+  puts "\n\nWelcome to TicTacToe, #{player_x} and #{player_o}!\n\n"
   9.times do |turn| # There are only 9 possible moves until draw
     player_turn = turn.even? ? 'X' : 'O'
-    puts "Turn for player #{player_turn}-> Select where you want to move: "
+    puts "\n\n\t\t\t\t|-|-|-|\n\t\t\t\t|-|-|-|\n\t\t\t\t|-|-|-|\n"
+    print "\n\nTurn for #{player_turn}-> Choose your move by using the numbers from 1 to 9 meaning left to right cells in descending order: "
     move_selection = gets.chomp
-    puts 'Your move selection is valid!'
+    puts "\n\nYour move selection is valid!\n\n"
     # All the board manipulation and checks happen here
-    puts "Your move selection was #{move_selection}."
-    puts 'Showing board.'
-    puts 'Your move is not winning... yet! But it\'s also not a draw.'
+    puts "\n\nYour move selection was #{move_selection}."
+    puts "\n\n\t\t\t\t|-|-|-|\n\t\t\t\t|-|-|-|\n\t\t\t\t|-|-|-|\n"
+    puts "\n\nYour move is not winning... yet! But it\'s also not a draw."
   end
 end
 
