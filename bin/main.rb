948cbc538ab44fed950777d25a71b6eb8f9fa8a4
Josias Alvarado <josiasjag@gmail.com>
2020-11-16 08:42:22 -0600

    Add changes as per TSE's requests.

--- bin/main.rb
+++ bin/main.rb
@@ -2,12 +2,19 @@
 
 def game_flow
   puts 'Tic Tac Toe Game Started!'
+  puts 'Enter name for player X: '
+  player_x = gets.chomp
+  puts 'Enter name for player O: '
+  player_o = gets.chomp
   9.times do |turn| # There are only 9 possible moves until draw
-    puts "Turn #{turn}-> Select where you want to move: "
+    player_turn = turn.even? ? "X" : "O"
+    puts "Turn for player #{player_turn}-> Select where you want to move: "
     move_selection = gets.chomp
+    puts 'Your move selection is valid!'
     # All the board manipulation and checks happen here
     puts "Your move selection was #{move_selection}."
     puts 'Showing board.'
+    puts 'Your move is not winning... yet! But it\'s also not a draw.'
   end
 end
 
