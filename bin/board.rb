cb4cd73a1983d234ee6043f3a8c1e92655c05570
Josias Alvarado <josiasjag@gmail.com>
2020-11-16 14:06:04 -0600

    Complete #win? method for the Board class.

--- bin/board.rb
+++ bin/board.rb
@@ -1,6 +1,5 @@
 class Board
-  attr_accessor :board, :turn, :rows, :game_over,
-                :columns, :diagonal, :diagonal_rev
+  attr_accessor :board, :turn, :game_over, :columns, :diagonal, :diagonal_rev
 
   def initialize
     # [[-, -, -],
@@ -9,34 +8,39 @@ class Board
     @board = Array.new(3) { Array.new(3, 0) }
     @turn = 0
     @game_over = false
-    @rows = [@board[0, 3], @board[3, 3], @board[6, 3]]
-    @columns = [[@board[0, 0], @board[1, 0], @board[2, 0]],
-               [@board[0, 1], @board[1, 1], @board[2, 1]],
-               [@board[0, 2], @board[1, 2], @board[2, 2]]]
-    @diagonal = [@board[0, 2], @board[1, 1], @board[2, 0]]
-    @diagonal_rev = [@board[0, 0], @board[1, 1], @board[2, 2]]
+  end
+
+  def columns
+    [[@board[0][0], @board[1][0], @board[2][0]],
+     [@board[0][1], @board[1][1], @board[2][1]],
+     [@board[0][2], @board[1][2], @board[2][2]]]
+  end
+
+  def diagonal
+    [@board[0][2], @board[1][1], @board[2][0]]
+  end
+
+  def diagonal_rev
+    [@board[0][0], @board[1][1], @board[2][2]]
   end
 
   def move(move:, player:)
     @turn += 1
-    if %w(X O).include?(@board[move[0]][move[1]])
+    if %w(X O).include?(@board[move[0]][move[1]]) == false
       @board[move[0]][move[1]] = player
       'Your move was a valid move!'
     else
       'Your move is illegal since that square is already taken.'
+    end
   end
 
   def win?(player:)
     winning = false
-    for row in rows
-      for inner_row in row
-        return true if inner_row.all?(String) && inner_row.all?(player)
-      end
+    for row in @board
+      return true if row.all?(String) && row.all?(player)
     end
     for column in columns
-      for inner_column in column
-        return true if inner_column.all?(String) && inner_column.all?(player)
-      end
+      return true if column.all?(String) && column.all?(player)
     end
     if @diagonal.all?(String) && @diagonal.all?(player)
       return true
@@ -48,4 +52,3 @@ class Board
     winning
   end
 end
-
--- bin/main.rb
+++ bin/main.rb
@@ -22,3 +22,4 @@ def game_flow
 end
 
 game_flow
+
